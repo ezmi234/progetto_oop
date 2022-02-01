a779c0a7711f2870f6e4a61619b4a11472b2f2dc
Ezmiron <ezmiron@MacBook-Air-di-Ezmiron.local>
2022-02-01 10:10:02 +0100

    Initializing Repo

---
+++ oop_project/oop_project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
